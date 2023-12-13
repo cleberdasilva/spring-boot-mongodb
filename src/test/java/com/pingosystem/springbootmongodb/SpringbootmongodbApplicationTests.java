010aee4329317ab23c3b1302db131e4ee5b8e737
cleber da silva <cleber.supertux@gmail.com>
2023-12-13 16:26:50 -0300

    First Commit - project created

---
+++ src/test/java/com/pingosystem/springbootmongodb/SpringbootmongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pingosystem.springbootmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootmongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
