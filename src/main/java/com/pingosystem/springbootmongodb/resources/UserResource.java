97bf344f04c50d138e37f0757195845a67738c4f
cleber da silva <cleber.supertux@gmail.com>
2023-12-14 15:39:06 -0300

    Using DTO pattern to return users

--- src/main/java/com/pingosystem/springbootmongodb/resources/UserResource.java
+++ src/main/java/com/pingosystem/springbootmongodb/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.pingosystem.springbootmongodb.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pingosystem.springbootmongodb.domain.User;
+import com.pingosystem.springbootmongodb.dto.UserDTO;
 import com.pingosystem.springbootmongodb.services.UserService;
 
 @RestController
@@ -19,11 +21,14 @@ public class UserResource {
 	private UserService userService;
 
 	@RequestMapping(method = RequestMethod.GET)
-	public ResponseEntity<List<User>> findAll(){
+	public ResponseEntity<List<UserDTO>> findAll(){
 		
 		//List é uma interface -> ArrayList é uma implementação do List
 		List<User> list = userService.findAll();
 		
-		return ResponseEntity.ok().body(list);
+		//convert every object into a DTO
+		List<UserDTO> listDto = list.stream().map(obj -> new UserDTO(obj)).collect(Collectors.toList());
+		
+		return ResponseEntity.ok().body(listDto);
 	}
 }
