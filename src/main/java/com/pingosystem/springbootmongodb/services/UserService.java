095301ab74e454b6f7d6513935f22a776bcd606b
cleber da silva <cleber.supertux@gmail.com>
2023-12-14 16:16:46 -0300

    Retrieving a user by id

--- src/main/java/com/pingosystem/springbootmongodb/services/UserService.java
+++ src/main/java/com/pingosystem/springbootmongodb/services/UserService.java
@@ -1,12 +1,15 @@
 package com.pingosystem.springbootmongodb.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 import com.pingosystem.springbootmongodb.domain.User;
 import com.pingosystem.springbootmongodb.repository.UserRepository;
+import com.pingosystem.springbootmongodb.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +19,10 @@ public class UserService {
 
 	public List<User> findAll(){
 		return userRepository.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = userRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found."));
 	}
 }
