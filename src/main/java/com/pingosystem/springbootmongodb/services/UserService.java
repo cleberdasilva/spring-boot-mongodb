fad5f941769c94e3c60843113c4a6ec84a2a11ae
cleber da silva <cleber.supertux@gmail.com>
2023-12-15 16:40:11 -0300

    Creating user with post

--- src/main/java/com/pingosystem/springbootmongodb/services/UserService.java
+++ src/main/java/com/pingosystem/springbootmongodb/services/UserService.java
@@ -4,10 +4,10 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 import com.pingosystem.springbootmongodb.domain.User;
+import com.pingosystem.springbootmongodb.dto.UserDTO;
 import com.pingosystem.springbootmongodb.repository.UserRepository;
 import com.pingosystem.springbootmongodb.services.exception.ObjectNotFoundException;
 
@@ -24,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = userRepository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found."));
+	}
+	
+	public User insert(User obj) {
+		return userRepository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
