28f52f3904aa0927349963f338d1c99d97c2586d
cleber da silva <cleber.supertux@gmail.com>
2023-12-19 16:02:09 -0300

    Retrieving a post by id

---
+++ src/main/java/com/pingosystem/springbootmongodb/services/PostService.java
@@ -0,0 +1,23 @@
+package com.pingosystem.springbootmongodb.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.pingosystem.springbootmongodb.domain.Post;
+import com.pingosystem.springbootmongodb.repository.PostRepository;
+import com.pingosystem.springbootmongodb.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository postRepository;
+
+	public Post findById(String id) {
+		Optional<Post> obj = postRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found."));
+	}
+	
+}
