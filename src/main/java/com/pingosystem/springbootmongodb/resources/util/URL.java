fbad2468496094e24107fcc478095020c8ee4baf
cleber da silva <cleber.supertux@gmail.com>
2023-12-20 11:30:52 -0300

    adding some comments

--- src/main/java/com/pingosystem/springbootmongodb/resources/util/URL.java
+++ src/main/java/com/pingosystem/springbootmongodb/resources/util/URL.java
@@ -7,7 +7,7 @@ public class URL {
 
 	public static String decodeParam(String text) {
 		try {
-			return URLDecoder.decode(text, "UTF-8");
+			return URLDecoder.decode(text, "UTF-8"); //decode text goog%20morning to == goog morning
 		} catch (UnsupportedEncodingException e) {
 			return "";
 		}
