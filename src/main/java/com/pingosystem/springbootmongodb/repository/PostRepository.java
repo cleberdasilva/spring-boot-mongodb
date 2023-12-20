462eca7d35e14a84d6700acf0c4f16aec36c9017
cleber da silva <cleber.supertux@gmail.com>
2023-12-20 11:44:50 -0300

    Simple queries with @Query

--- src/main/java/com/pingosystem/springbootmongodb/repository/PostRepository.java
+++ src/main/java/com/pingosystem/springbootmongodb/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.pingosystem.springbootmongodb.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.pingosystem.springbootmongodb.domain.Post;
@@ -10,6 +11,10 @@ import com.pingosystem.springbootmongodb.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
  //the CRUD is included
+	
+	//?0 says that I´ll get the first parameter i -> case-insensitive
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }") 
+	List<Post> searchTitle(String text);
 	
 	List<Post> findByTitleContainingIgnoreCase(String text); //Ignoring UPPER or DOW Case
 }
